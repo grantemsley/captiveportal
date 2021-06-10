25a4ff266654a9a473449c3fb2c9074d1f9dc288
Grant Emsley <grant@emsley.ca>
2021-06-10 11:55:58 -0400

    Setup static files

--- captiveportal/captiveportal/settings.py
+++ captiveportal/captiveportal/settings.py
@@ -15,6 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
 
 
@@ -33,7 +35,6 @@ ALLOWED_HOSTS = ['linuxtest']
 
 INSTALLED_APPS = [
     'voucher.apps.VoucherConfig',
-    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
